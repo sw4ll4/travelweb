2dd18a04e12cdc26abc17f97f693fcf29f95f30f
sw4ll4 <saleh.bhzpour@gmail.com>
2025-08-21 14:52:55 +0330

    header complete

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links")
+const menuBtnIcon = document.querySelector("#menu-btn i")
+
+
+menuBtn.addEventListener("click", (e)=>{
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-large-line" : "ri-menu-2-fill" );
+
+});
